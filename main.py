183e747f9d0d8649c2d16b93f53e42954e877d8e
princealexandre <princealexandre2000@gmail.com>
2025-06-24 23:04:37 -0400

    Edited the main.py file to import from pygame and create a black screen

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import pygame
 
 from constants import *
 
+screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0, 0, 0,))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
