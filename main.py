b5945edbb2d327f659abdc5995c3ca0cc96b8329
princealexandre <princealexandre2000@gmail.com>
2025-07-01 20:47:19 -0400

    created a file to allow to draw the player of the asteroids file, and added the required stuff in main.py and constants.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
@@ -13,8 +15,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0, 0, 0,))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+x = SCREEN_WIDTH/2
+y = SCREEN_HEIGHT/2
+player = Player(x,y)
 
 if __name__ == "__main__":
     main()
